50718dff3f6806747f9ec634f0b830fdd0ed5829
josrojrom1 <josrojrom@alum.us.es>
2026-03-30 14:50:16 +0200

    PFO-13: [Insertado operador ? en el modelo de profileData y recuperado el estilo scss de los bordes en los titulos de secciones]

--- src/app/models/profile/profile-data.ts
+++ src/app/models/profile/profile-data.ts
@@ -1,17 +1,17 @@
 export interface ProfileData {
-    fullName: string;
-    jobTitle: string;
-    avatarUrl: string;
-    bio: string;
-    skills: {
-        languages: string;
-        frameworks: string;
-        softSkills: string;
-        tools: string;
+    fullName?: string;
+    jobTitle?: string;
+    avatarUrl?: string;
+    bio?: string;
+    skills?: {
+        languages?: string;
+        frameworks?: string;
+        softSkills?: string;
+        tools?: string;
     };
-    education: {
-        university: string;
-        degree: string;
-        languages: string;
+    education?: {
+        university?: string;
+        degree?: string;
+        languages?: string;
     };
 }
