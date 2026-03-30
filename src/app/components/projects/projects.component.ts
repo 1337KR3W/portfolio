139b57467b3c346332b1061f9feffaf718afaad9
josrojrom1 <josrojrom@alum.us.es>
2026-03-30 21:42:36 +0200

    PFO-18: [Reajuste en ancho de las secciones]

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,13 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonGrid, IonRow, IonCol, IonCardSubtitle, IonButton, IonIcon } from "@ionic/angular/standalone";
-import { Project } from 'src/app/models/project/project.model';
 
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
-  imports: [IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonRow, IonCol, IonCardSubtitle, IonButton, IonIcon],
+  imports: [IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonRow, IonCol, IonCardSubtitle, IonButton, IonIcon, IonGrid],
 })
 export class ProjectsComponent {
 
