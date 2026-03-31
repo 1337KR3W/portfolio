4c468aa75637951b94f0081bb211631a910b3366
josrojrom1 <josrojrom@alum.us.es>
2026-03-31 12:38:48 +0200

    PFO-12: [Actualizar CSS global]

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { IonGrid, IonRow, IonCol } from "@ionic/angular/standalone";
+import { IonGrid, IonRow, IonCol, IonCard } from "@ionic/angular/standalone";
 import { Skill } from 'src/app/models/skill/skill.model';
 
 
@@ -8,7 +8,7 @@ import { Skill } from 'src/app/models/skill/skill.model';
   standalone: true,
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss'],
-  imports: [IonGrid, IonRow, IonCol],
+  imports: [IonGrid, IonRow, IonCol, IonCard],
 })
 export class SkillsComponent {
   @Input({ required: true }) data: any[] = [];
