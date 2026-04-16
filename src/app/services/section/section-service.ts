83c80062390ee65582a57a605c4bed2de27a2e4d
josrojrom1 <josrojrom@alum.us.es>
2026-04-16 22:31:53 +0200

    Agregados nuevos proyectos turbobtainer y virtualKnowledge y agregados nuevas skills

--- src/app/services/section/section-service.ts
+++ src/app/services/section/section-service.ts
@@ -24,8 +24,8 @@ export class SectionService {
         avatarUrl: './assets/avatar.gif',
         bio: 'Graduated in Software Engineering at the University of Seville (US) at the Higher Technical School of Computer Engineering (ETSII). I have liked computers since I was a child and I am currently professionally dedicated to it, particularly the development of hybrid solutions for mobile phones. I enjoy my free time making music, hosting dedicated servers of some video games and always learning something new.',
         skills: {
-          languages: 'Java, Python, TypeScript, Javascript, SQL, Bash',
-          frameworks: 'Angular, Ionic, Spring Boot, Django, Flask, Node.js',
+          languages: 'Java, Python, TypeScript, Javascript, SQL, Bash, Rust',
+          frameworks: 'Angular, Ionic, Spring Boot, Django, Flask, Node.js, Tauri',
           styling: 'Bootstrap, SCSS, Tailwind, HTML',
           tools: 'Docker, Capacitor, GitHub/GitLabs, Jira, Postman, NVM',
           os: 'Windows, Linux, VirtualBox',
@@ -62,14 +62,15 @@ export class SectionService {
       description: 'Heres a small list with my projects:',
       component: ProjectsComponent,
       extraData: [
-        { id: 1, title: 'Acme Platform', subtitle: 'Java, Maven, Spring Boot', description: 'A platform for students and teachers...', imgUrl: './assets/image/projects/acme.png', gitUrl: 'https://github.com/1337KR3W/DP2-G27-Acme' },
-        { id: 2, title: 'Virtual Knowledge', subtitle: 'Angular, Material Design', description: 'Web app for managing user data using ReqRes API.', imgUrl: './assets/image/projects/virtual-knowledge.png', gitUrl: 'https://github.com/1337KR3W/Virtualknowledge-Frontend' },
+        { id: 1, title: 'Turbobtainer', subtitle: 'Rust, Tauri, Angular, Ionic', description: 'Tool for downloading media.', imgUrl: './assets/image/projects/turbobtainer.png', gitUrl: 'https://github.com/1337KR3W/turbobtainer' },
+        { id: 2, title: 'Virtual Knowledge', subtitle: 'Angular, Ionic, Typescript, Java, Spring Boot, SQL', description: 'Web app for managing projects and tasks.', imgUrl: './assets/image/projects/virtual-knowledge.png', gitUrl: 'https://github.com/1337KR3W/Virtualknowledge-Frontend' },
         { id: 3, title: 'getyt', subtitle: 'Python, PyTube, Tkinter', description: 'YouTube streams tool.', imgUrl: './assets/image/projects/getyt.png', gitUrl: 'https://github.com/1337KR3W/getyt' },
         { id: 4, title: 'Tkinter timer', subtitle: 'Python, Tkinter', description: 'Timer using Tkinter.', imgUrl: './assets/image/projects/tkinter-timer.png', gitUrl: 'https://github.com/1337KR3W/pyside6_timer' },
         { id: 5, title: 'PySide6 timer', subtitle: 'Python, PySide6', description: 'Timer using PySide6.', imgUrl: './assets/image/projects/pyside6-timer.png', gitUrl: 'https://github.com/1337KR3W/DP2-G27-Acme' },
         { id: 6, title: 'Flet timer', subtitle: 'Python, Flet', description: 'Simple timer using Flet.', imgUrl: './assets/image/projects/flet-timer.png', gitUrl: 'https://github.com/1337KR3W/flet_timer' },
-        { id: 7, title: 'Netscanner', subtitle: 'Bash, Arp, Python', description: 'Tool to discover connected hosts via arp packets.', imgUrl: './assets/image/projects/netscanner.png', gitUrl: 'https://github.com/1337KR3W/netscanner' },
-        { id: 8, title: 'Pynotech', subtitle: 'Python, PySide6', description: 'Notepad developed for my Bachelors Thesis.', imgUrl: './assets/image/projects/pynotech.png', gitUrl: 'https://github.com/1337KR3W/pyNotech' }
+        { id: 7, title: 'Acme Platform', subtitle: 'Java, Maven, Spring Boot', description: 'A platform for students and teachers...', imgUrl: './assets/image/projects/acme.png', gitUrl: 'https://github.com/1337KR3W/DP2-G27-Acme' },
+        { id: 8, title: 'Netscanner', subtitle: 'Bash, Arp, Python', description: 'Tool to discover connected hosts via arp packets.', imgUrl: './assets/image/projects/netscanner.png', gitUrl: 'https://github.com/1337KR3W/netscanner' },
+        { id: 9, title: 'Pynotech', subtitle: 'Python, PySide6', description: 'Notepad developed for my Bachelors Thesis.', imgUrl: './assets/image/projects/pynotech.png', gitUrl: 'https://github.com/1337KR3W/pyNotech' }
       ]
     },
     {
